116f26e874bbf5084758c4a06898c80ad0e94df8
amsamu <samaropulido@gmail.com>
2023-05-04 22:32:48 +0200

    Enabled data binding and can access nav menu

--- app/src/main/java/com/amsamu/mymedialists/MainActivity.java
+++ app/src/main/java/com/amsamu/mymedialists/MainActivity.java
@@ -1,14 +1,38 @@
 package com.amsamu.mymedialists;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.amsamu.mymedialists.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    public ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Launching main activity
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Navigation menu
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, binding.drawerLayout, R.string.nav_menu_open, R.string.nav_menu_close);
+        binding.drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
